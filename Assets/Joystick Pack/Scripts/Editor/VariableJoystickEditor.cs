8e05dccf428616fec96fa8aee6e4a82415de94e7
kingvamp <114408599@qq.com>
2019-11-28 19:13:51 +0800

    Assets commit

---
+++ Assets/Joystick Pack/Scripts/Editor/VariableJoystickEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+[CustomEditor(typeof(VariableJoystick))]
+public class VariableJoystickEditor : JoystickEditor
+{
+    private SerializedProperty moveThreshold;
+    private SerializedProperty displayType;
+    private SerializedProperty joystickType;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        moveThreshold = serializedObject.FindProperty("moveThreshold");
+        displayType = serializedObject.FindProperty("displayType");
+        joystickType = serializedObject.FindProperty("joystickType");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (background != null)
+        {
+            RectTransform backgroundRect = (RectTransform)background.objectReferenceValue;
+            backgroundRect.pivot = center;
+        }
+    }
+
+    protected override void DrawValues()
+    {
+        base.DrawValues();
+        EditorGUILayout.PropertyField(moveThreshold, new GUIContent("Move Threshold", "The distance away from the center input has to be before the joystick begins to move."));
+        EditorGUILayout.PropertyField(displayType, new GUIContent("Display Type", "摇杆显示类型"));
+        EditorGUILayout.PropertyField(joystickType, new GUIContent("Joystick Type", "The type of joystick the variable joystick is current using."));
+    }
+}
---
+++ Assets/Joystick Pack/Scripts/Joysticks/VariableJoystick.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VariableJoystick : Joystick
+{
+    public float MoveThreshold { get { return moveThreshold; } set { moveThreshold = Mathf.Abs(value); } }
+
+    [SerializeField] private float moveThreshold = 1;
+    [SerializeField] private DisplayType displayType = DisplayType.AutoHide;
+    [SerializeField] private JoystickType joystickType = JoystickType.Fixed;
+
+    private Vector2 fixedPosition = Vector2.zero;
+
+    public void SetMode(JoystickType joystickType)
+    {
+        this.joystickType = joystickType;
+        if (joystickType == JoystickType.Fixed)
+        {
+            background.anchoredPosition = fixedPosition;
+        }
+
+        switch (displayType)
+        {
+            case DisplayType.Show: background.gameObject.SetActive(true); break;
+            case DisplayType.AutoHide: background.gameObject.SetActive(false); break;
+            case DisplayType.Hide: background.gameObject.SetActive(false); break;
+        }
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        fixedPosition = background.anchoredPosition;
+        SetMode(joystickType);
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        if (joystickType != JoystickType.Fixed)
+        {
+            background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+            if (displayType == DisplayType.AutoHide) background.gameObject.SetActive(true);
+        }
+        base.OnPointerDown(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        if (displayType == DisplayType.AutoHide) background.gameObject.SetActive(false);
+        base.OnPointerUp(eventData);
+    }
+
+    protected override void HandleInput(float magnitude, Vector2 normalised, Vector2 radius, Camera cam)
+    {
+        if (joystickType == JoystickType.Dynamic && magnitude > moveThreshold)
+        {
+            Vector2 difference = normalised * (magnitude - moveThreshold) * radius;
+            background.anchoredPosition += difference;
+        }
+        base.HandleInput(magnitude, normalised, radius, cam);
+    }
+}
+
+public enum JoystickType { Fixed, Floating, Dynamic }
+
+public enum DisplayType { Show, AutoHide, Hide }
